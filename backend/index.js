2565467a3a149248b5a28c8e3db3584c172b4276
Praveen Raj <praveenraj7667@gmail.com>
2025-07-05 22:33:29 +0530

    solved cookies related error

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,19 @@ dotenv.config({});
 const app = express();
 const port = 3000;
 
-//middleware
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
-app.use(cookieParser());
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL, 
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-//apis
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/expense", expenseRoute);
 
@@ -30,6 +32,6 @@ app.listen(port, () => {
   connectDb();
 });
 
-app.get("/", (req, res) => {
-  res.send("Welcome this is root route");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome this is root route");
+// });
