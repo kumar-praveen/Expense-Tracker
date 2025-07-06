706294ee606e635ee6c89caabfdc6dd332ac74fb
Praveen Raj <praveenraj7667@gmail.com>
2025-07-06 21:43:09 +0530

    solving token related problem

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -40,6 +40,7 @@ function Login() {
         }
       );
       if (res.data.success) {
+        console.log(res.data.token)
         localStorage.setItem("token", res.data.token);
         toast.success(res.data.message);
         dispatch(setAuthUser(res.data.user));
